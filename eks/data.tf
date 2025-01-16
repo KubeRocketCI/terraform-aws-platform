0c24cc8f0b7a8fb5a6d9d8eda973a590dc3149cd
Mykola Serdiuk <mykola_serdiuk@epam.com>
2025-01-16 16:45:42 +0200

    [EPMDEDP-14574]: feat: Add Karpenter IAM template

--- eks/data.tf
+++ eks/data.tf
@@ -13,3 +13,54 @@ data "aws_security_group" "default" {
 data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
 }
+
+data "aws_iam_policy" "AmazonEC2ContainerRegistryReadOnly" {
+  name = "AmazonEC2ContainerRegistryReadOnly"
+}
+
+data "aws_iam_policy" "AmazonEKS_CNI_Policy" {
+  name = "AmazonEKS_CNI_Policy"
+}
+
+data "aws_iam_policy" "AmazonEKSWorkerNodePolicy" {
+  name = "AmazonEKSWorkerNodePolicy"
+}
+
+data "aws_iam_policy" "AmazonSSMManagedInstanceCore" {
+  name = "AmazonSSMManagedInstanceCore"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+  }
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement           {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect = "Allow"
+    principals {
+      type       = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${module.eks.oidc_provider}"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${module.eks.cluster_oidc_issuer_url}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+
+    condition {
+      test     = "StringEquals"
+      variable = "${module.eks.cluster_oidc_issuer_url}:sub"
+      values   = ["system:serviceaccount:karpenter:karpenter"]
+    }
+  }
+}
