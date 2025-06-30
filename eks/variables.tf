399fab438e75f4f25be1789972f68fc1701935d8
Daniil Nedostup <daniil_nedostup@epam.com>
2025-06-30 21:03:32 +0300

    [EPMDEDP-15349]: chore: Add IRSA roles for Argo CD and CD Pipeline Operator

--- eks/variables.tf
+++ eks/variables.tf
@@ -159,6 +159,40 @@ variable "cluster_identity_providers" {
   default     = {}
 }
 
+variable "create_cd_pipeline_operator_irsa" {
+  type    = bool
+  default = false
+}
+
+variable "create_argocd_irsa" {
+  type    = bool
+  default = false
+}
+
+variable "cd_pipeline_operator_agent_role_arn" {
+    description = "ARN of the CD Pipeline Operator Agent IAM role in account B"
+    type        = string
+    default     = ""
+}
+
+variable "cd_pipeline_operator_irsa_role_arn" {
+  description = "ARN of the CD Pipeline Operator IRSA role in account A"
+  type        = string
+  default     = ""
+}
+
+variable "argocd_agent_role_arn" {
+  description = "ARN of the ArgoCD Agent IAM role in account B"
+  type        = string
+  default     = ""
+}
+
+variable "argocd_irsa_role_arn" {
+  description = "ARN of the ArgoCD IRSA role in account A"
+  type        = string
+  default     = ""
+}
+
 # Atlantis IAM Role variables
 variable "create_atlantis_iam_role" {
   description = "Enable or disable the creation of IAM role for Atlantis"
